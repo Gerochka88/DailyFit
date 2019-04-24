eae3012f0bed06a59d6e775baa4f5af73990c41e
Taras Vitoshko <taras.vitoshko@aiesec.net>
2019-04-24 14:42:07 +0300

    created AddBodyCondition, fixed bugs with Macaw cocoapods

--- DailyFit/Controller/AddNewBodyConditionVC.swift
+++ DailyFit/Controller/AddNewBodyConditionVC.swift
@@ -9,6 +9,53 @@
 import Foundation
 import UIKit
 
-class AddNewBodyConditionVC: UIViewController {
+class AddNewBodyConditionVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var txtWeight: UITextField!
+    @IBOutlet weak var txtHeight: UITextField!
+    @IBOutlet weak var txtChest: UITextField!
+    @IBOutlet weak var txtBiceps: UITextField!
+
+    @IBAction func btnAddPicture(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose photo source", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action: UIAlertAction) in
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo library", style: .default, handler: { (action: UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func btnSave(_ sender: Any) {
+    let weight = txtWeight.text
+    let height = txtHeight.text
+    let chest = txtChest.text
+    let biceps = txtBiceps.text
+        
+    var bodyConditions = [BodyCondition]()
+        
+    bodyConditions = [BodyCondition(date: Date(), weight: weight, height: height, chest: chest, biceps: biceps, image: UIImage(named: "BodyCondition"))]
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
 }
