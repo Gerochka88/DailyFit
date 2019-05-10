d8416dd7422a6e7874155ad4afd681e0d4a0dab5
Taras Vitoshko <taras.vitoshko@aiesec.net>
2019-05-10 21:29:15 +0300

    Added View Controller for Total of steps

---
+++ DailyFit/Controller/TotalOfStepsVC.swift
@@ -0,0 +1,21 @@
+//
+//  TotalOfStepsVC.swift
+//  DailyFit
+//
+//  Created by Taras Vitoshko on 5/10/19.
+//  Copyright © 2019 Taras Vitoshko. All rights reserved.
+//
+
+import UIKit
+
+class TotalOfStepsVC: UIViewController {
+    
+
+    
+    @IBOutlet weak var totalOfStepsView: TotalOfStepsView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        totalOfStepsView.play()
+}
+
+}
