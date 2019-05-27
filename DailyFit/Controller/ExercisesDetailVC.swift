31499d2a305422eba24f3b642c4dd76a69aa8110
Taras Vitoshko <taras.vitoshko@aiesec.net>
2019-05-27 22:49:10 +0300

    added TableView cell for exercises table view

---
+++ DailyFit/Controller/ExercisesDetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  ExercisesDetailVC.swift
+//  DailyFit
+//
+//  Created by Taras Vitoshko on 5/27/19.
+//  Copyright © 2019 Taras Vitoshko. All rights reserved.
+//
+
+import UIKit
+
+class ExercisesDetailVC: UIViewController {
+    @IBOutlet weak var exercisesSubtitleView: UILabel!
+    @IBOutlet weak var exercisesPictureView: UIImageView!
+    @IBOutlet weak var exercisesTitleLabel: UILabel!
+    @IBOutlet weak var exercisesDecriptionTextView: UITextView!
+    
+    var exercise: ExercisesModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setUI(){
+        exercisesPictureView.image = exercise?.image
+        exercisesTitleLabel.text = exercise?.title
+        exercisesSubtitleView.text = exercise?.subTitle
+        exercisesDecriptionTextView.text = exercise?.description
+        
+    }
+
+}
---
+++ DailyFit/View/ExercisesTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ExercisesTableViewCell.swift
+//  DailyFit
+//
+//  Created by Taras Vitoshko on 5/27/19.
+//  Copyright © 2019 Taras Vitoshko. All rights reserved.
+//
+
+import UIKit
+
+class ExercisesTableViewCell: UITableViewCell {
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var exercisesImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        cardView.layer.shadowOpacity = 1
+        cardView.layer.shadowOffset = CGSize.zero
+        cardView.layer.shadowColor = UIColor.darkGray.cgColor
+        cardView.layer.cornerRadius = 10
+        exercisesImage.roundCorner(radius: 10)
+    }
+    func setup(exerciseModel: ExercisesModel)
+    {
+        titleLabel.text = exerciseModel.title
+        subtitleLabel.text = exerciseModel.subTitle
+        exercisesImage.image = exerciseModel.image
+    }
+}
