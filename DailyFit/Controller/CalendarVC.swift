91d321eb4cf52a69a497cfe5e849e2f89dc25b7c
Taras Vitoshko <taras.vitoshko@aiesec.net>
2019-05-30 00:08:32 +0300

    Added calendar(need to be fixed) and some new images

---
+++ DailyFit/Controller/CalendarVC.swift
@@ -0,0 +1,202 @@
+/*
+ * ViewController.swift
+ * Created by Michael Michailidis on 01/04/2015.
+ * http://blog.karmadust.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+import UIKit
+import EventKit
+
+class CalendarVC: UIViewController, CalendarViewDataSource, CalendarViewDelegate {
+
+    
+
+    @IBOutlet weak var calendarView: CalendarView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        CalendarView.Style.cellShape                = .bevel(8.0)
+        CalendarView.Style.cellColorDefault         = UIColor.clear
+        CalendarView.Style.cellColorToday           = UIColor(red:1.00, green:0.84, blue:0.64, alpha:1.00)
+        CalendarView.Style.cellSelectedBorderColor  = UIColor(red:1.00, green:0.63, blue:0.24, alpha:1.00)
+        CalendarView.Style.cellEventColor           = UIColor(red:1.00, green:0.63, blue:0.24, alpha:1.00)
+        CalendarView.Style.headerTextColor          = UIColor.white
+        CalendarView.Style.cellTextColorDefault     = UIColor.white
+        CalendarView.Style.cellTextColorToday       = UIColor.orange
+        
+        CalendarView.Style.firstWeekday             = .sunday
+        
+        CalendarView.Style.locale                   = Locale(identifier: "en_US")
+        
+        CalendarView.Style.timeZone                 = TimeZone(abbreviation: "UTC")!
+        
+        CalendarView.Style.hideCellsOutsideDateRange = false
+        CalendarView.Style.changeCellColorOutsideRange = false
+        
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        
+        calendarView.direction = .horizontal
+        calendarView.multipleSelectionEnable = false
+        calendarView.marksWeekends = true
+        
+        
+        calendarView.backgroundColor = UIColor(red:0.31, green:0.44, blue:0.47, alpha:1.00)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        let today = Date()
+        
+        var tomorrowComponents = DateComponents()
+        tomorrowComponents.day = 1
+        
+        
+        let tomorrow = self.calendarView.calendar.date(byAdding: tomorrowComponents, to: today)!
+        self.calendarView.selectDate(tomorrow)
+
+        #if KDCALENDAR_EVENT_MANAGER_ENABLED
+        self.calendarView.loadEvents() { error in
+            if error != nil {
+                let message = "The karmadust calender could not load system events. It is possibly a problem with permissions"
+                let alert = UIAlertController(title: "Events Loading Error", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        #endif
+        
+        
+        self.calendarView.setDisplayDate(today)
+        
+        self.datePicker.locale = CalendarView.Style.locale
+        self.datePicker.timeZone = CalendarView.Style.timeZone
+        self.datePicker.setDate(today, animated: false)
+    }
+
+    // MARK : KDCalendarDataSource
+    
+    func startDate() -> Date {
+        
+        var dateComponents = DateComponents()
+        dateComponents.month = -3
+        
+        let today = Date()
+        
+        let threeMonthsAgo = self.calendarView.calendar.date(byAdding: dateComponents, to: today)!
+        
+        return threeMonthsAgo
+    }
+    
+    func endDate() -> Date {
+        
+        var dateComponents = DateComponents()
+      
+        dateComponents.year = 2
+        let today = Date()
+        
+        let twoYearsFromNow = self.calendarView.calendar.date(byAdding: dateComponents, to: today)!
+        
+        return twoYearsFromNow
+  
+    }
+    
+    
+    // MARK : KDCalendarDelegate
+   
+    func calendar(_ calendar: CalendarView, didSelectDate date : Date, withEvents events: [CalendarEvent]) {
+        
+        print("Did Select: \(date) with \(events.count) events")
+        for event in events {
+            print("\t\"\(event.title)\" - Starting at:\(event.startDate)")
+        }
+        
+    }
+    
+    func calendar(_ calendar: CalendarView, didScrollToMonth date : Date) {
+        
+        self.datePicker.setDate(date, animated: true)
+    }
+    
+    
+    func calendar(_ calendar: CalendarView, didLongPressDate date : Date, withEvents events: [CalendarEvent]?) {
+        
+        if let events = events {
+            for event in events {
+                print("\t\"\(event.title)\" - Starting at:\(event.startDate)")
+            }
+        }
+        
+        let alert = UIAlertController(title: "Create New Event", message: "Message", preferredStyle: .alert)
+        
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Event Title"
+        }
+        
+        let addEventAction = UIAlertAction(title: "Create", style: .default, handler: { (action) -> Void in
+            let title = alert.textFields?.first?.text
+         //   self.calendarView.addEvent(title!, date: date)
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        alert.addAction(addEventAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    // MARK : Events
+    
+    @IBAction func onValueChange(_ picker : UIDatePicker) {
+        self.calendarView.setDisplayDate(picker.date, animated: true)
+    }
+    
+    @IBAction func goToPreviousMonth(_ sender: Any) {
+        self.calendarView.goToPreviousMonth()
+    }
+    @IBAction func goToNextMonth(_ sender: Any) {
+        self.calendarView.goToNextMonth()
+        
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
+
+
+
+
+
+
